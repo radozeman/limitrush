d80b6f28f0520e0dd702bf4bf3cc3ca4ff6229ef
radozeman <r.zeman96@gmail.com>
2024-09-18 22:16:17 +0200

    setup serving build for deploy

--- server/app.ts
+++ server/app.ts
@@ -1,6 +1,8 @@
 import { Hono } from "hono";
 import { logger } from "hono/logger";
 import { workoutsRoute } from "./routes/workouts";
+import { serveStatic } from "hono/bun";
+
 const app = new Hono();
 
 app.use("*", logger());
@@ -10,5 +12,8 @@ app.get("/", (c) => {
 });
 
 app.route("/api/workouts", workoutsRoute);
+
+app.use("*", serveStatic({ root: "./client/dist" }));
+app.get("*", serveStatic({ path: "./client/dist/index.html" }));
 
 export default app;
