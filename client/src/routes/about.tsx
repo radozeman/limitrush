30869fd705461332b8d76ca62f9d3605d9f8da03
radozeman <r.zeman96@gmail.com>
2024-09-24 19:28:10 +0200

    add tanstack forms

--- client/src/routes/about.tsx
+++ client/src/routes/about.tsx
@@ -1,5 +1,5 @@
 import { createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/about")({
-  component: () => <div className="p-8 bg-red-400"> Hello /about!</div>,
+  component: () => <div> Hello /about!</div>,
 });
